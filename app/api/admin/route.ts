22000129eed591c0b2cdeffe83e718e75021e98f
sundicode <sundicharles2003@mail.com>
2023-11-24 16:11:46 +0100

    updates made to user admin and schedules routes

--- app/api/admin/route.ts
+++ app/api/admin/route.ts
@@ -1,2 +1,7 @@
-export async function GET() {}
+import { NextResponse as res } from "next/server";
+export async function GET() {
+  return res.json({
+    message: "hello",
+  });
+}
 export async function POST() {}
