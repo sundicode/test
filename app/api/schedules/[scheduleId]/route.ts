c40cbb86731afd2536eff382a69d5cf86bc2bd66
sundicode <sundicharles2003@mail.com>
2023-11-24 21:56:39 +0100

    updates made to user admin and schedules routes

--- app/api/schedules/[scheduleId]/route.ts
+++ app/api/schedules/[scheduleId]/route.ts
@@ -1,7 +1,10 @@
-import { NextResponse as res } from "next/server";
+import { NextRequest, NextResponse as res } from "next/server";
 export async function PATCH({ params }: { params: { scheduleId: string } }) {
   return res.json({ id: params.scheduleId }, { status: 200 });
 }
-export async function DELETE({ params }: { params: { scheduleId: string } }) {
+export async function DELETE(
+  req: NextRequest,
+  { params }: { params: { scheduleId: string } }
+) {
   return res.json({ id: params.scheduleId }, { status: 200 });
 }
