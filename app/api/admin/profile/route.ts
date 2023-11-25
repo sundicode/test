4da1b4fa11243023334b8ea12c45bcf593e54ea9
sundicode <sundicharles2003@mail.com>
2023-11-25 06:38:07 +0100

    updates made to user admin and schedules routes

--- app/api/admin/profile/route.ts
+++ app/api/admin/profile/route.ts
@@ -1,1 +1,14 @@
-export async function GET() {}
+import { errorCodes } from "@/utils/errorCode";
+import { NextRequest, NextResponse as res } from "next/server";
+
+export async function GET(req: NextRequest) {
+  const origin = req.headers.get("origin");
+  try {
+    return res.json({});
+  } catch (error: any) {
+    return res.json(
+      { error: error?.message },
+      { status: errorCodes.serverError }
+    );
+  }
+}
